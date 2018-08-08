d1e2aa6e844989e676ad7c1f603095e18d81e702
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2018-08-08 20:49:11 +0530

    function2

---
+++ M9/Functions and Objects Exercise-2/functions_and_objects_2.py
@@ -0,0 +1,23 @@
+#Exercise : Function and Objects Exercise-2
+#Implement a function that converts the given testList = [1, -4, 8, -9] into [2, -3, 9, -8]
+
+
+def inc (k):
+   return k+1
+
+def apply_to_each(L, f):
+   for [i, _] in enumerate(L):
+       L[i] = f(L[i])
+   return L
+
+def main():
+   data = input()
+   data = data.split()
+   list1 = []
+   for j in data:
+       list1.append(int(j))
+   print(apply_to_each(list1, inc))
+
+if __name__ == "__main__":
+   main()
+ 
