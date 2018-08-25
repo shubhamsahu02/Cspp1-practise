3daf1910432fddd8180ce228044cb194a426c7bf
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2018-08-26 00:00:43 +0530

    hand

--- M14/Classes and Objects - Coordinate Exercise/coordinate_exercise.py
+++ M14/Classes and Objects - Coordinate Exercise/coordinate_exercise.py
@@ -15,25 +15,24 @@ class Coordinate(object):
 	def getY(self):
 		# Getter method for a Coordinate object's y coordinate
 		return self.y
-
         
 	def __str__(self):
 		return '<' + str(self.getX()) + ',' + str(self.getY()) + '>'
 
-	def __eq__(self, other)
-		return self.x == other.y
-	def __repr__(self)
-	    return 'Coordinate(' + str(self.getX()) + ',' + str(self.getY()) + ')'	#Coordinate(2,3)
 # Your task is to define the following two methods for the Coordinate class:
 # Add an __eq__ method that returns True if coordinates refer to same point in the plane (i.e., have the same x and y coordinate).
 # Define __repr__, a special method that returns a string that looks like a valid Python expression that could be used to recreate an
 # object with the same value. In other words, eval(repr(c)) == c given the definition of __eq__ from part 1.
 
+	def __eq__(self, other)
+	return self.getX() == other.getX() and self.getY() == other.getY()
 
+	def __repr__(self):
+		return '<' + str(self.getX()) + ',' + str(self.getY()) + '>'
 
 
 def main():
-	data = raw_input()
+	data = input()
 	data = data.split(' ')
 	data = list(map(int, data))
 	print(Coordinate(data[0],data[1]) == Coordinate(data[2],data[3]))
